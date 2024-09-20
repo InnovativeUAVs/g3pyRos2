c132bf9730d25d660845b66f6625348acc667e4f
fadarnell <innovation@apeironuav.com>
2024-09-20 10:12:37 -0400

    initial copies of structure from fs2020

---
+++ ros2/README.md
@@ -0,0 +1,38 @@
+Collect metrics from ROS2, using ROS2 subscriptions.
+
+MSFS provides many [SimVars](https://docs.flightsimulator.com/html/index.htm?#t=Programming_Tools%2FSimVars%2FSimulation_Variables.htm)
+which are mapped to G3 gauges via the mapping defined in `mapping.yml`.
+SimVars units should be discovered automatically and tranformed to
+match your specified gauge units.
+
+
+You can test your mapping by starting up your publishers and running.
+
+```sh
+python -m ros2.fetch
+```
+
+You should see a dump of the currently mapped metrics:
+
+ROS2CONNECT_EXCEPTION_UNRECOGNIZED_ID: in (b'ENG N1 RPM:1', b'Rpm (0 to 16384 = 0 to 100%)')
+ROS2CONNECT_EXCEPTION_UNRECOGNIZED_ID: in (b'ENG MANIFOLD PRESSURE:1', b'inHG.')
+
+```json
+{
+    "airspeed:Knots": 0.0,
+    "altitude:Feet": 108.50824737548828,
+    "compass:Degrees": 229.14576721191406,
+    "cylinderHeadTemp:Rankine": 781.202915998182,
+    "engineRPM:Rpm (0 to 16384 = 0 to 100%)": null,
+    "heading:Radians": 3.998244922257538,
+    "manifoldPressure:inHG.": null,
+    "oilPressure:foot pounds": 270863.0178080027,
+    "oilTemperature:Rankine": 628.7004895557371,
+    "pitch:Radians": -0.011671489104628563,
+    "pressureSetting:Millibars": 1009.2831420898438,
+    "roll:Radians": 0.000904008926145336,
+    "slip:Position": -0.0078125,
+    "turnrate:Radians per second": 7.495487400080828e-07,
+    "verticalSpeed:feet/minute": -0.03363388823345297
+}
+```
---
+++ ros2/fetch.py
@@ -0,0 +1,138 @@
+from typing import Dict, List, Optional, Any
+import json
+from os import path
+from ruamel.yaml import YAML
+#from simconnect import SimConnect, PERIOD_VISUAL_FRAME
+# add ros2 imports
+import logging
+from time import sleep
+
+
+# default interval for running background task
+background_frequency_seconds = 1
+
+
+yaml = YAML(typ='safe')
+
+mydir = path.dirname(__file__)
+with open(path.join(mydir, 'units.yml')) as f:
+    units = yaml.load(f)
+
+unitmap = {}
+for std, simunits in units.items():
+    if not isinstance(simunits, list):
+        simunits = [simunits]
+    for u in simunits:
+        unitmap[u.lower()] = std
+
+with open(path.join(mydir, 'mapping.yml')) as f:
+    cfg = yaml.load(f)
+    mapping = cfg['metrics']
+    actions = cfg['actions']
+
+
+# Create SimConnect link
+sc = SimConnect()
+dd = sc.subscribe_simdata(
+    [dict(name=m['simvar'], units=m.get('unit')) for m in mapping],
+    period=PERIOD_VISUAL_FRAME,
+    interval=10,
+)
+simvars = dd.get_units()
+
+# update mapping with actual simvar units
+for m in mapping:
+    if m['simvar'] in simvars:
+        unit = simvars[m['simvar']]
+        m['unit'] = unitmap.get(unit.lower(), unit)
+    else:
+        m.setdefault('unit', None)
+        logging.warning(f"g3py:fs2020:WARNING: No simvar for {m['simvar']}")
+
+latest = 0
+
+
+def background_task() -> None:
+    # logging.debug("exhausting simconnect queue")
+    while sc.receive():
+        pass
+
+
+def pollMetrics(metrics: Optional[List[str]] = None) -> Dict[str, Any]:
+    global latest
+    background_task()
+    recent = dd.simdata.changedsince(latest)
+    latest = dd.simdata.latest()
+    data = {}
+    for m in mapping:
+        name = m['metric']
+        sv = m['simvar']
+        if metrics and name not in metrics or sv not in recent:
+            continue
+        v = recent[sv]
+        if 'fx' in m:
+            v = eval(m['fx'], None, dict(x=v))
+        data[name] = v
+    return data
+
+
+def metricUnits() -> Dict[str, str]:
+    """Return the effective unit for each metric, overriding with fxunit if present"""
+    return {
+        m['metric']: m['fxunit'] if 'fxunit' in m else m['unit']
+        for m in mapping
+    }
+
+
+def triggerActions(inputs: Dict[str, Any]) -> Dict[str, Any]:
+    outputs = dict()
+    for k, v in inputs.items():
+        rules = [rule for rule in actions if rule.get('input') == k]
+        if not rules:
+            continue
+        for rule in rules:
+            if 'fx' in rule:
+                v = eval(rule['fx'], None, dict(x=v))
+            # simple copy from input to output
+            if 'output' in rule:
+                logging.debug(f"triggerActions: setting {rule['output']} to {v}")
+                outputs[rule['output']] = v
+            # set a simulation variable
+            elif 'simvar' in rule:
+                logging.debug(f"triggerActions: setting simvar {rule['simvar']} to {v}")
+                sc.set_simdatum(rule['simvar'], v)
+            # generate an event
+            elif 'event' in rule:
+                name = rule['event']
+                method = rule.get('method')
+                if method == 'incdec':
+                    name = name[0 if v > 0 else 1]
+                # trigger an inc / dec event a number of times
+                if method == 'incdec':
+                    v = abs(v)
+                    logging.debug(f"triggerActions: sending {v} x {name} events")
+                    while v > 0:
+                        sc.send_event(name)
+                        v -= 1
+                # trigger an event with a data value
+                elif method == 'set':
+                    logging.debug(f"triggerActions: sending event {name}({v})")
+                    sc.send_event(name, v)
+                # just trigger an event
+                else:
+                    logging.debug(f"triggerActions: sending event {name}")
+                    sc.send_event(name)
+            else:
+                logging.warning(f"triggerActions: malformed rule {rule}")
+    return outputs
+
+
+# block until first subscription update so we have valid initial metrics
+while not dd.simdata.keys():
+    background_task()
+    sleep(0.1)
+
+
+if __name__ == '__main__':
+    print(json.dumps(metricUnits(), indent=4, sort_keys=True))
+    print(json.dumps(pollMetrics(), indent=4, sort_keys=True))
